522e1effa1684c598bf5f3f89989b0378569fc76
venetrius <gergelygjuhasz@gmail.com>
2019-05-08 00:26:28 +0000

    Poppin' Bottles Strch part 1

---
+++ stretch/poppin-bottles/popping-bottles.js
@@ -0,0 +1,40 @@
+function updateOnBuy(numOfBottles, source){
+  details.bought  += numOfBottles;
+  details[source] += numOfBottles;
+  details.bottle  += numOfBottles;
+  details.cap     += numOfBottles;
+}
+
+
+
+function calcNumberOFBottles(money){
+  updateOnBuy(Math.floor(money / 2),"boughtByMoney");
+  console.log("first", details);
+  while(details.bottle >= 2 || details.cap >=4 ){
+    let {bottle, cap} = details;
+    console.log("bought", details.bought, "bottle", details.bottle, "cap", details.cap);
+    if(bottle >=2){
+      let toBuy = bottle % 2 == 0 ? (bottle / 2) : (bottle -1)/ 2;
+      details.bottle -= 2 * toBuy;
+      updateOnBuy(toBuy, "boughtByBottle");
+    }
+    else if(cap >=4){
+      let toBuy = Math.floor(cap / 4);
+      details.cap -= toBuy * 4;
+      updateOnBuy(toBuy, "boughtBycap");
+    }
+  }
+  console.log(details.bought);
+}
+
+let details =
+    {
+      bought          : 0,
+      bottle          : 0,
+      cap             : 0,
+      boughtByMoney   : 0,
+      boughtByBottle  : 0,
+      boughtBycap     : 0
+    };
+
+calcNumberOFBottles(40);
